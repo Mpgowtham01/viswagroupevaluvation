05b7bc50ff64ebf296fdd7efe6618229ff9733d1
Gowtham <Gowtham@cognextech.com>
2025-02-26 22:23:18 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import Formpage from "./Form";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App: React.FC = () => {
   return (
     <div>
-      <Formpage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Formpage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
